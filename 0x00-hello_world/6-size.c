d6defbc172080235e0cb6c27dcf58f164528bfe5
NkosanaMathaba <inkomathaba@icloud.com>
2023-02-19 04:19:54 -0800

    6. Sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int l;
+	float f;
+	long long z;
+
+	printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu\n", (unsigned long)sizeof(l));
+	printf("Size of a long long: %lu.\n", (unsigned long)sizeof(z));
+	printf("Size of a float: %lu.\n", (unsigned long)sizeof(f));
+	return (0);
+}
