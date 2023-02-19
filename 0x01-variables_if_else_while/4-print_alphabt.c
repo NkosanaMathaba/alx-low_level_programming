5b2b5709c9b13e48688a1feffbd0f62f82692f41
NkosanaMathaba <inkomathaba@icloud.com>
2023-02-19 12:35:05 -0800

    4. Print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet excluding p and e
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		putchar(c);
+	}
+
+	putchar('\n');
+}
