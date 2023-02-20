0c740f2620fb329ede902ecfb68b021c2ff801e5
NkosanaMathaba <inkomathaba@icloud.com>
2023-02-20 08:46:08 -0800

    100. Inventing fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,20 @@
 int main(void)
 {
 	int n;
+	int p;
 
 	for (n = 0 ; n < 100; n++)
 	{
-		putchar((n / 10) + '0');
-		putchar((n % 10) + '0');
-		putchar(',');
-		putchar(' ');
+		for (p = n + 1; p <= 9; p++)
+		{
+			putchar(n + '0');
+			putchar(p + '0');
+
+			if (n == 8 && p == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
