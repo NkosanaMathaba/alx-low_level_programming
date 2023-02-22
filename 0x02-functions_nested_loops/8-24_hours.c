85a29d33f942af850d5088170b7a697ec99b099b
NkosanaMathaba <inkomathaba@icloud.com>
2023-02-22 10:27:18 -0800

    8. Clockwork

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ */
+
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; x < 24; x++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			if (y < 60)
+			{
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+				_putchar(':');
+				_putchar((y / 10) + '0');
+				_putchar((y % 10) + '0');
+				_putchar('\n');
+			}
+		}
+	}
+}
