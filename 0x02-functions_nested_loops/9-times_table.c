024fc515b3e020aeb829ec0ccdc6b856a86c9d22
NkosanaMathaba <inkomathaba@icloud.com>
2023-02-22 12:03:56 -0800

    9. Times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table
+ */
+
+void times_table(void)
+{
+	int x, m, a;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar('0');
+		for (m = 1; m <= 9; m++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			a = x + m;
+
+			if (a < 10)
+				_putchar(' ');
+			else
+				_putchar((a / 10) +'0');
+			_putchar((a % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
