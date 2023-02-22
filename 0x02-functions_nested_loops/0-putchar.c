5c452c3481c99a2876158f183eea120c40c29fed
NkosanaMathaba <inkomathaba@icloud.com>
2023-02-22 03:57:22 -0800

    0. Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - Program will print _putchar
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	int i = '_';
+	int i1 = 'p';
+	int i2 = 'u';
+	int i3 = 't';
+	int i4 = 'c';
+	int i5 = 'h';
+	int i6 = 'a';
+	int i7 = 'r';
+
+	putchar(i);
+	putchar(i1);
+	putchar(i2);
+	putchar(i3);
+	putchar(i4);
+	putchar(i5);
+	putchar(i6);
+	putchar(i7);
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# Start of Functions and nested Loops
+
+Lets stay *MOTIVATED* to push and keep __DOING HARD THINGS__
