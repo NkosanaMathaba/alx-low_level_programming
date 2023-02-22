f39560e55921b919975851dc9930747db0805037
NkosanaMathaba <inkomathaba@icloud.com>
2023-02-22 05:23:36 -0800

    3. Islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - check for lowercase c
+ *
+ * Return: (1) if c is lowercase
+ * Retrun: (0) otherwise
+ */
+
+int _islower(int c)/* char - */
+{
+
+	_putchar(c);
+	return (0);
+}
