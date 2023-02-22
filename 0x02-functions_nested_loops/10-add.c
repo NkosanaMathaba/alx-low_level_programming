43e46371f0b902bff2fe33ad25b63cc3f0e9a7c4
NkosanaMathaba <inkomathaba@icloud.com>
2023-02-22 12:28:15 -0800

    10. Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns a result
+ * @x: first integer
+ * @y: Second integer
+ * Return: returns result
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
