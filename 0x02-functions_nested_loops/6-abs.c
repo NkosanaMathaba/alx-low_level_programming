4d776565d1babb34b6a4e8ed05b680d324785c71
NkosanaMathaba <inkomathaba@icloud.com>
2023-02-22 07:59:36 -0800

    6. abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute integer
+ *@x: integer I will be computing
+ * Return: Always (0) Success
+ */
+
+int _abs(int x)
+{
+	if (x <= 0)
+		return (x);
+	else
+		return (-x);
+}
