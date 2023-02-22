ab98b1d0f187ae760297752e0a3085bcbb753c76
NkosanaMathaba <inkomathaba@icloud.com>
2023-02-22 15:03:28 -0800

    13 nature

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Succes
+ */
+
+int main(void)
+{
+	int x, sum = 0;
+	
+	for (x = 0; x < 1024; x++)
+	{
+		if ((x % 3) == 0 || (x % 5) == 0)
+			sum += x;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
