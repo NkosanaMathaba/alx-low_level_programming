1e7aaadea6d8d3849f3dcb4db5dedc8b71d7c232
NkosanaMathaba <inkomathaba@icloud.com>
2023-02-27 09:24:51 -0800

    0. reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer to an int
+ *
+ * @n: integer being checked
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# Beginning of pointers, strings and arrays
+Discipline comes from __DOING HARD THINGS__
