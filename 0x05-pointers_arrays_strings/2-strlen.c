0f323e59ab52fc235c78788893d187ac8603f5ad
NkosanaMathaba <inkomathaba@icloud.com>
2023-02-27 09:54:18 -0800

    2. string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of the string
+ *
+ * @s: character being checked
+ *
+ * return: (0)
+ */
+
+int _strlen(char *s)
+{
+	int x = 0;
+
+	for (; *s++;)
+		x++;
+	return (x);
+}
