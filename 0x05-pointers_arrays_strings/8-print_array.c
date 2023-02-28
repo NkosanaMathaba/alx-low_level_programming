72cc7ac59d011196ec622cfed698eed94a606219
NkosanaMathaba <inkomathaba@icloud.com>
2023-02-28 12:08:45 -0800

    8. Print

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - prints an array
+ *
+ * @a: integer
+ * @n: array
+ */
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+		if (x != n - 1)
+			printf("%d, ", a[x]);
+		else
+			print("%d, ", a[x]);
+	printf("\n");
+}
