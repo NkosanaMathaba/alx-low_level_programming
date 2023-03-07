427a431a39c044b320b4c910cc1c690a2ce32baa
NkosanaMathaba <inkomathaba@icloud.com>
2023-03-07 12:18:47 -0800

    0. Memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - fills the memory of a constant byte
+ *
+ * @n: bytes of memory pointed to by s
+ * @s: constant byte
+ * @b: memory area pointer
+ *
+ * Return: a pointer tothe memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x = 0;
+	
+	while (x < n)
+	{
+		s[x] = b;
+		x++;
+	}
+	return (s);
+}
