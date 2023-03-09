768a6a2e1531ca57c1a2e2d8d6aa33bb776ebc80
NkosanaMathaba <inkomathaba@icloud.com>
2023-03-08 22:28:17 -0800

    0. Puts Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function prints a string in reverse
+ *
+ * @s: Character string
+ *
+ */
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+# Start on C Programming recursion
+
+We honor the work by  __DOING THE HARD THINGS__
