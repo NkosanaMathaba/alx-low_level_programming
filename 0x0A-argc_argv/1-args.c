24fa7bee8db53d8314056a62c327ff81a187b311
NkosanaMathaba <inkomathaba@icloud.com>
2023-03-13 12:37:20 -0700

    1. args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * @argc: Argument Count
+ *
+ * Return: Always (0) Success
+ */
+
+int main(int argc)
+{
+	printf("%d\n", argc);
+	return (0);
+}
