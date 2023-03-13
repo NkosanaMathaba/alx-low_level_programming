444ed851ec68c92f2b7e932107d3713c050ef8be
NkosanaMathaba <inkomathaba@icloud.com>
2023-03-13 12:31:08 -0700

    0. Fixed betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,13 @@
 /**
  * main - Entry Point
  *
- * argv: argument vector
+ * @argc: Argument Count
+ * @argv: Argument Vector
  *
  * Return: Always (0)
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int x;
 
