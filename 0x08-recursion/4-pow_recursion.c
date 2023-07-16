fe9d62df7bd2a129d4bb0f5124457dc2cfdbe5d8
NkosanaMathaba <inkomathaba@icloud.com>
2023-07-16 11:12:58 -0700

    4. pow4. pow4. pow4. pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints the power of an number
+ * @x: integer number
+ * @y: power of integer x
+ *
+ * Return: the value of x raised to thepower of y.
+ * returns -1 if y less than 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if ( y != 0)
+		return (x * _pow_recursion(x, (y - 1)));
+	else
+		return (1);
+}
